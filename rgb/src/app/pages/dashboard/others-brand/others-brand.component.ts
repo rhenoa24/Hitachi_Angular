c16364a53012cc9eec77fcba093c599968d72bee
Alyssa D. Bautista <alyssarhenoanicolebautista@gmail.com>
2026-06-12 00:01:19 +0800

    created page-header.css for others-brand.component (reusable)

---
+++ rgb/src/app/pages/dashboard/others-brand/others-brand.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-others-brand',
+  imports: [],
+  templateUrl: './others-brand.component.html',
+  styleUrl: './others-brand.component.css',
+})
+export class OthersBrandComponent {}
